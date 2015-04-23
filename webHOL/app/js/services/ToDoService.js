c3cf7ae7fe5410680e090bf519470dcc091f56b6
Cory Fowler <cfowler@microsoft.com>
2015-04-22 19:48:37 -0700

    adding webHOL

---
+++ webHOL/app/js/services/ToDoService.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+var apiPath = "http://mobilehol-code.azurewebsites.net/tables";
+multiChannelToDoApp
+    .factory('toDoService', ['$http', function ($http) {
+        return {
+
+            getItems: function () {
+                return $http.get(apiPath + '/TodoItem');
+            },
+
+            add: function (id, task) {
+                return $http.post(apiPath + '/TodoItem', { "id": id + 1, "text": task, "complete": false });
+            },
+
+            complete: function (item) {
+                return $http.put(apiPath + '/TodoItem/' + item.Id, { "id": item.Id, "text": item.Text, "complete": true });
+            }
+        }
+    }]);
